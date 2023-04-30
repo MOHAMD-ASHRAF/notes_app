18a9817b1f37e2898acc5b378e49c9d91700660a
mohamed ashraf <moashrafasd12@gmail.com>
2023-04-30 15:35:05 +0300

    add color for sheet done

--- lib/cubits/add_ note_cubit/add_note_cubit.dart
+++ lib/cubits/add_ note_cubit/add_note_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive/hive.dart';
 import 'package:notes_app/constants.dart';
@@ -7,8 +8,10 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+  Color color = const Color(0xff96adc8);
 
   addNote(NoteModel note) async {
+    note.color = color.value;
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>(kNotesBox);
